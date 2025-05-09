440061f1a34731bb00b94739727ca12e2ea26be1
Om Chaudhary <165839109+Om9137@users.noreply.github.com>
2025-05-09 19:31:28 +0530

    Add files via upload

---
+++ Basic Mathematical Operations & Personalized Greeting.py
@@ -0,0 +1,17 @@
+
+### Task 1: Perform Basic Mathematical Operations
+
+a=int(input("Enter the Num_1:" ))
+b=int(input("Enter the Num_2:" ))
+print('Addition is:',a+b)
+print('Subtraction is:',a-b)
+print('Multiplication is:',a*b)
+print('Division is:',a//b)
+
+
+
+### Task 2: Create a Personalized Greeting
+
+First=input('Type First Name:')
+Second=input('Type Surname:')
+print("Hello!",First+" "+Second ,"\nWelcome to my first Python project.")
