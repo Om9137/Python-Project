1c8bb34cf5a5f2dec6f6f6eb7b87a79ebd2b3cbe
Om Chaudhary <165839109+Om9137@users.noreply.github.com>
2025-05-11 13:20:02 +0530

    Add files via upload

---
+++ Control Structure in python.py
@@ -0,0 +1,19 @@
+# # Task 1: Check if a Number is Even or Odd
+
+A=int(input("Enter the Number: "))
+
+if A/2==0:
+    print("This is the even number.")
+else:
+    print("This is the odd number.")
+
+
+
+
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+sum=0
+for i in range(1,51):
+    sum = sum+i
+print("The sum of num from 1 to 50 is: ",sum)
+
